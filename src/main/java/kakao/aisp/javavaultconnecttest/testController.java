4a86e38c36756b72f7f9e4b9dbf2fc0a706e0e28
curiousKidd <skasrjhaan@gmail.com>
2024-03-24 21:53:00 +0900

    feignClient update

--- src/main/java/kakao/aisp/javavaultconnecttest/testController.java
+++ src/main/java/kakao/aisp/javavaultconnecttest/testController.java
@@ -2,22 +2,26 @@ package kakao.aisp.javavaultconnecttest;
 
 import kakao.aisp.javavaultconnecttest.model.resquest.VaultRequestDTO;
 import kakao.aisp.javavaultconnecttest.serviceClient.VaultFeignClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/test")
+@RequiredArgsConstructor
 public class testController {
 
-    private VaultFeignClient vaultFeignClient;
+    private final VaultFeignClient vaultFeignClient;
 
-    @Value("${spring.cloud.app-role.role-id}")
+    @Value("${spring.cloud.vault.app-role.role-id}")
     private String roleId;
 
-    @Value("${spring.cloud.app-role.role-id}")
+    @Value("${spring.cloud.vault.app-role.secret-id}")
     private String secretId;
 
-    @GetMapping
+    @GetMapping("/token")
     public void test() {
 
         VaultRequestDTO vaultRequestDTO = VaultRequestDTO.builder()
