7ad63e6495c99314dced150899de442df916c183
curiousKidd <skasrjhaan@gmail.com>
2024-03-26 19:47:56 +0900

    readme update

--- README.md
+++ README.md
@@ -14,3 +14,7 @@ vault:1.13.3 -> docker를 통해서 설정해보려 하였으나 1일차 장렬�
 2일차 -> docker compose 를 활용한 local vault 설치는 완료하였으나, vault 설정하는 부분은 실패함... yml 파일 업데이트
 3일차 -> 드디어 vault 설정 및 통신 테스트 완료... postman을 통한
      -> 이후 feign client 활용하여 접속 api 만들기 진행중
+
+5일차 -> api 테스트 이후 feign client 테스트 완료
+     -> 로그인 token 은 정상적으로 가져옴
+     -> 이후 data를 가져오는 api 작성중
--- src/main/java/kakao/aisp/javavaultconnecttest/serviceClient/VaultFeignClient.java
+++ src/main/java/kakao/aisp/javavaultconnecttest/serviceClient/VaultFeignClient.java
@@ -5,11 +5,15 @@ import kakao.aisp.javavaultconnecttest.model.DTO.resquest.VaultRequestDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "vault", url = "http://localhost:8200/v1")
 public interface VaultFeignClient {
 
     @PostMapping(path = "/auth/approle/login", produces = "application/json")
     VaultResponseDTO getAuthToken(@RequestBody VaultRequestDTO dto);
+
+    @PostMapping(path = "secret/data/secret_test", produces = "application/json")
+    VaultResponseDTO getSecretData(@RequestHeader("X-Vault-Token") String token);
 
 }
